56fd5226fa766d0194bb2dfa8ac2cb2bc275b281
Roman <Polew.Rom@gmail.com>
2021-09-12 17:52:22 +0300

    Задача 2. Определение констант.

---
+++ test2.php
@@ -0,0 +1,7 @@
+<?php
+    const PICTURES = 80;
+    define('MARKERS', 23);
+    const PENCILS = 40;
+    $paints = PICTURES - MARKERS - PENCILS;
+    echo "Красками нарисовано ". $paints . " рисунков.\n";
+
